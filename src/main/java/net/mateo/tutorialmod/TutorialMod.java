9b8c2e5a377074b7425c5f2fd8a6ded8455cd9e8
MateoMazziniOngay <mateomazzini610@gmail.com>
2023-09-14 16:00:58 -0300

    Deleted unnecessary comments.

--- src/main/java/net/mateo/tutorialmod/TutorialMod.java
+++ src/main/java/net/mateo/tutorialmod/TutorialMod.java
@@ -19,9 +19,7 @@ import org.slf4j.Logger;
 @Mod(TutorialMod.MOD_ID)
 public class TutorialMod
 {
-    // Define mod id in a common place for everything to reference
     public static final String MOD_ID = "tutorialmod";
-    // Directly reference a slf4j logger
     private static final Logger LOGGER = LogUtils.getLogger();
 
     public TutorialMod()
