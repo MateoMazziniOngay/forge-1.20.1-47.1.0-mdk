af15bad533ff9f4e64321e717fd58fb3488edcc4
MateoMazziniOngay <mateomazzini610@gmail.com>
2023-09-14 16:55:35 -0300

    Sapphire item added to the mod.

--- src/main/java/net/mateo/tutorialmod/TutorialMod.java
+++ src/main/java/net/mateo/tutorialmod/TutorialMod.java
@@ -1,6 +1,8 @@
 package net.mateo.tutorialmod;
 
 import com.mojang.logging.LogUtils;
+import net.mateo.tutorialmod.item.ModItems;
+import net.minecraft.world.item.CreativeModeTabs;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.BuildCreativeModeTabContentsEvent;
@@ -26,6 +28,8 @@ public class TutorialMod
     {
         IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
 
+        ModItems.register(modEventBus);
+
         modEventBus.addListener(this::commonSetup);
 
         MinecraftForge.EVENT_BUS.register(this);
@@ -39,7 +43,9 @@ public class TutorialMod
     // Add the example block item to the building blocks tab
     private void addCreative(BuildCreativeModeTabContentsEvent event)
     {
-
+        if(event.getTabKey() == CreativeModeTabs.INGREDIENTS) {
+            event.accept(ModItems.SAPPHIRE);
+        }
     }
 
     // You can use SubscribeEvent and let the Event Bus discover methods to call
